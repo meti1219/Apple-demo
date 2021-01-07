e27180156f902bcaa1a7075375bbf935ddf0e416
meti1219 <amotifikir@gmail.com>
2021-01-06 18:44:45 -0800

    update

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    if ($(window).width() <= 768) {
+        $('.footer-links-wrapper').addClass("someClass");
+    } else {
+        $('.footer-links-wrapper').removeClass("someClass");
+    }
+    $(window).on('resize', function (event) {
+        if ($(window).width() <= 768) {
+            $('.footer-links-wrapper').addClass("someClass");
+        } else {
+            $('.footer-links-wrapper').removeClass("someClass");
+            $('.footer-links-wrapper ul').show();
+        }
+    });
+
+    $(document).on("click", ".someClass h3", function() {
+        $(this).next('ul').slideToggle();
+        $(this).toggleClass("expanded");
+    });
+
+});
